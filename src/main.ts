084b18d38a1fe2dd6f3ffe7cab3f6df3b2e67d81
Thu Pham Xuan <xuanthu404@gmail.com>
2020-08-10 15:04:42 +0700

    feat: add OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api/v1')
+  app.setGlobalPrefix('/api/v1');
+  const options = new DocumentBuilder()
+    .setTitle('Bloxnest API')
+    .setDescription('This open API for Bloxnest')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', in: 'header', scheme: 'bearer', bearerFormat: 'Bearer ', name: 'Authorization' })
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
